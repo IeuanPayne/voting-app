5d04c7fff6209f712916287b485c0d698c8ae218
Your Name <you@example.com>
2021-04-15 13:16:44 +0100

    initial commit for new branch

---
+++ javascript/AreasofWellbeing1.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('submit-btn')
+
+startButton.addEventListener('click', startSelection)
+
+function startSelection(){
+  console.log('Working')
+  startButton.classList.add('areasofwellbeing')
+}
+
+function setNextStatement(){
+
+}
+
+function selectStatement(){
+
+}
